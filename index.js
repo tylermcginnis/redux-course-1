590604c44b5117843eaab35ea8f061799be504b3
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-01 20:05:38 -0700

    Code for reducer-function

--- index.js
+++ index.js
@@ -30,6 +30,21 @@
   id: 0
 }
 
+/*
+Characteristics of a Pure Function
+1) They always return the same result if the same arguments are passed in.
+2) They depend only on the arguments passed into them.
+3) Never produce any side effects.
+*/
+
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
 function createStore () {
   // The store should have four parts
   // 1. The state
