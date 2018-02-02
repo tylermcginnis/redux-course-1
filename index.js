f3bc8a68484907f47096176ed243abdd7f0d6b2a
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-01 20:07:06 -0700

    Code for more-actions

--- index.js
+++ index.js
@@ -39,11 +39,18 @@ Characteristics of a Pure Function
 
 // Reducer function
 function todos (state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
+  switch(action.type) {
+    case 'ADD_TODO' :
+      return state.concat([action.todo])
+    case 'REMOVE_TODO' :
+      return state.filter((todo) => todo.id !== action.id)
+    case 'TOGGLE_TODO' :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, {complete: !todo.complete})
+      )
+    default :
+      return state
   }
-
-  return state
 }
 
 function createStore (reducer) {
