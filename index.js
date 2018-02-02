7d021a1596017fa2f74d731ada84f5b67dbbc9fd
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-01 20:05:02 -0700

    Code for cs-updating-state

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+{
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false,
+  }
+}
+
+{
+  type: 'REMOVE_TODO',
+  id: 0,
+}
+
+{
+  type: 'TOGGLE_TODO',
+  id: 0,
+}
+
+{
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Run a Marathon'
+  }
+}
+
+{
+  type: 'REMOVE_GOAL',
+  id: 0
+}
+
 function createStore () {
   // The store should have four parts
   // 1. The state
