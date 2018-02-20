bfecabe224ca3b73bd7d5825094f90adea2b7a35
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-20 13:21:48 -0700

    Code for folder-store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import reducer from './reducers'
+import middleware from './middleware'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
--- src/middleware/checker.js
+++ src/middleware/checker.js
@@ -1,3 +1,6 @@
+import { ADD_TODO } from '../actions/todos'
+import { ADD_GOAL } from '../actions/goals'
+
 const checker = (store) => (next) => (action) => {
   if (
     action.type === ADD_TODO &&
