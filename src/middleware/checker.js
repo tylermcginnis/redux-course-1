46efbff5fbae2fdc99fbd4b7e799a3e93272d84f
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-02-20 13:01:23 -0700

    Code for folder-middleware

---
+++ src/middleware/checker.js
@@ -0,0 +1,19 @@
+const checker = (store) => (next) => (action) => {
+  if (
+    action.type === ADD_TODO &&
+    action.todo.name.toLowerCase().indexOf('bitcoin') !== -1
+  ) {
+    return alert("Nope. That's a bad idea.")
+  }
+
+  if (
+    action.type === ADD_GOAL &&
+    action.goal.name.toLowerCase().indexOf('bitcoin') !== -1
+  ) {
+    return alert("Nope. That's a bad idea.")
+  }
+
+  return next(action)
+}
+
+export default checker
